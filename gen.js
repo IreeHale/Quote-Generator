d9712a417819b9ed1bd63735dc6a82ed041cfddd
Iree Hale <LightWatch21@protonmail.com>
2021-01-04 18:56:25 -0700

    fixing api bug

--- gen.js
+++ gen.js
@@ -1,8 +1,9 @@
 //Get a Quote From API
 async function getQoute() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/method=getQoute&lang=en&format=json';
     try{
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         console.log(data);
     } catch (error) {
